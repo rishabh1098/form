3c9cdc90cb831d9cd7112312bfa6e1c0ea32e0ee
rishabh1098 <rishabhwadhwa39@gmail.com>
2021-02-12 14:40:41 +0530

    firstcommit

--- formdetails/models.py
+++ formdetails/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class studentdetails(models.Model):
+    student_id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=60)
+    school=models.CharField(max_length=60)
+    board=models.CharField(max_length=60)
+    standard=models.CharField(max_length=20)
+    address=models.CharField(max_length=50)
+    category=models.CharField(max_length=200,default="")
+    number=models.CharField(max_length=20)
+    registerdate = models.DateTimeField(default=timezone.now())
--- website/views.py
+++ website/views.py
@@ -5,8 +5,25 @@ from email.mime.text import MIMEText
 import json
 from datetime import datetime
 import re
+from formdetails.models import studentdetails
 
 
 def index(request):
+    if request.method == "POST":
+        name = request.POST.get('name', '')
+        school = request.POST.get('school', '')
+        board = request.POST.get('board', '')
+        standard = request.POST.get('standard', '')
+        address = request.POST.get('address', '')
+        category = request.POST.get('category', '')
+        number = request.POST.get('number', '')
+        registerdate = datetime.now()
+        data = studentdetails(name=name, number=number, school=school,
+                              board=board, standard=standard, address=address, category=category, registerdate=registerdate)
+        data.save()
+        print(name)
+        feedbackupdate = [
+            {'text': 'Your response has been recieved!'}]
+        response = json.dumps(feedbackupdate, default=str)
+        return HttpResponse(response)
     return render(request, 'index.html')
-
